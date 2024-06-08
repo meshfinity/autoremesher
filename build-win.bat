ca874f702224444ced12acfb2134fc0a0f22ebc8
iffyloop <35245220+iffyloop@users.noreply.github.com>
2024-06-08 21:19:34 +1000

    Change drive letter for GitHub Actions

--- build-win.bat
+++ build-win.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "D:\a\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 set AUTOREMESHER_BUILD_DIR=%~dp0
 
@@ -33,25 +33,25 @@ cd %AUTOREMESHER_BUILD_DIR%
 
 curl -O https://archives.boost.io/release/1.66.0/source/boost_1_66_0.zip
 tar -xf boost_1_66_0.zip
-mkdir C:\Libraries
-echo D | xcopy boost_1_66_0 C:\Libraries\boost_1_66_0 /s /e /q /y
+mkdir D:\a\Libraries
+echo D | xcopy boost_1_66_0 D:\a\Libraries\boost_1_66_0 /s /e /q /y
 
 cd thirdparty/openvdb/openvdb-7.0.0
 mkdir build
 cd build
-cmake -G "Visual Studio 17 2022" -A x64 -D "OPENVDB_CORE_STATIC=OFF" -D "OPENVDB_BUILD_VDB_PRINT=OFF" -D "IlmBase_INCLUDE_DIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/openexr/openexr-2.4.1" -D "IlmBase_Half_LIBRARY=%AUTOREMESHER_BUILD_DIR%/thirdparty/openexr/openexr-2.4.1/build/IlmBase/Half/Release/Half-2_4.lib" -D "TBB_INCLUDEDIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/tbb/include" -D "TBB_LIBRARYDIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/tbb/build2/Release" -D "ZLIB_INCLUDE_DIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/zlib/zlib-1.2.11" -D "ZLIB_LIBRARY=%AUTOREMESHER_BUILD_DIR%/thirdparty/zlib/zlib-1.2.11/build/Release/zlibstatic.lib" -D "Blosc_INCLUDE_DIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/blosc/c-blosc-1.18.1" -D "Blosc_LIBRARY=%AUTOREMESHER_BUILD_DIR%/thirdparty/blosc/c-blosc-1.18.1/build/blosc/Release/libblosc.lib" -D "BOOST_INCLUDEDIR=C:/Libraries/boost_1_66_0" -D "OPENVDB_DISABLE_BOOST_IMPLICIT_LINKING=ON" -D "CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES=C:/Libraries/boost_1_66_0;%AUTOREMESHER_BUILD_DIR%/thirdparty/blosc/c-blosc-1.18.1/blosc;%AUTOREMESHER_BUILD_DIR%/thirdparty/zlib/zlib-1.2.11/build" ../
+cmake -G "Visual Studio 17 2022" -A x64 -D "OPENVDB_CORE_STATIC=OFF" -D "OPENVDB_BUILD_VDB_PRINT=OFF" -D "IlmBase_INCLUDE_DIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/openexr/openexr-2.4.1" -D "IlmBase_Half_LIBRARY=%AUTOREMESHER_BUILD_DIR%/thirdparty/openexr/openexr-2.4.1/build/IlmBase/Half/Release/Half-2_4.lib" -D "TBB_INCLUDEDIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/tbb/include" -D "TBB_LIBRARYDIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/tbb/build2/Release" -D "ZLIB_INCLUDE_DIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/zlib/zlib-1.2.11" -D "ZLIB_LIBRARY=%AUTOREMESHER_BUILD_DIR%/thirdparty/zlib/zlib-1.2.11/build/Release/zlibstatic.lib" -D "Blosc_INCLUDE_DIR=%AUTOREMESHER_BUILD_DIR%/thirdparty/blosc/c-blosc-1.18.1" -D "Blosc_LIBRARY=%AUTOREMESHER_BUILD_DIR%/thirdparty/blosc/c-blosc-1.18.1/build/blosc/Release/libblosc.lib" -D "BOOST_INCLUDEDIR=D:/a/Libraries/boost_1_66_0" -D "OPENVDB_DISABLE_BOOST_IMPLICIT_LINKING=ON" -D "CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES=D:/a/Libraries/boost_1_66_0;%AUTOREMESHER_BUILD_DIR%/thirdparty/blosc/c-blosc-1.18.1/blosc;%AUTOREMESHER_BUILD_DIR%/thirdparty/zlib/zlib-1.2.11/build" ../
 cmake --build . --config Release
 cd %AUTOREMESHER_BUILD_DIR%
 
-mkdir C:\Qt
-cd C:\Qt
+mkdir D:\a\Qt
+cd D:\a\Qt
 curl -O -L https://github.com/miurahr/aqtinstall/releases/download/Continuous/aqt.exe
 aqt install-qt windows desktop 5.13.2 win64_msvc2017_64
 cd %AUTOREMESHER_BUILD_DIR%
 
-set QTDIR=C:\Qt\5.13.2\msvc2017_64
+set QTDIR=D:\a\Qt\5.13.2\msvc2017_64
 set PATH=%PATH%;%QTDIR%\bin
-qmake "BOOST_INCLUDEDIR=C:\Libraries\boost_1_66_0" "CGAL_DIR=%AUTOREMESHER_BUILD_DIR%\thirdparty\cgal\CGAL-5.1-beta1"
+qmake "BOOST_INCLUDEDIR=D:\a\Libraries\boost_1_66_0" "CGAL_DIR=%AUTOREMESHER_BUILD_DIR%\thirdparty\cgal\CGAL-5.1-beta1"
 nmake -f Makefile.Release
 
 copy thirdparty\openvdb\openvdb-7.0.0\build\openvdb\Release\openvdb.dll release\openvdb.dll
